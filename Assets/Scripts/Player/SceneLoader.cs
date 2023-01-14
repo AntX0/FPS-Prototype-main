f87e3c84dd5a13e7c2cafc888549fff87263ae62
Anton Pan <antoxaantoxa23@gmail.com>
2023-01-14 18:01:31 +0600

    Final adjustments

--- Assets/Scripts/Player/SceneLoader.cs
+++ Assets/Scripts/Player/SceneLoader.cs
@@ -6,7 +6,7 @@ public class SceneLoader : MonoBehaviour
 {
     public void ReloadGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
         Time.timeScale= 1.0f;
         Cursor.visible= false;
         FindObjectOfType<CameraShake>().GetComponent<CinemachineVirtualCamera>().m_Lens.Dutch = 0;
